ed0a1c2566e0e90ecbbe4ec8531f477a4b46653d
Michael Clark <michael.clark@syliant.com>
2017-03-14 11:53:26 -0400

    fixed some merge problems

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 import express from 'express';
-<<<<<<< HEAD
-import { apolloServer } from 'apollo-server';
-import schema from './data/schema.graphql';
-import Mocks from './data/mocks';
-=======
 import Schema from './data/schema';
 import Resolvers from './data/resolvers';
 //import Mocks from './data/mocks';
@@ -11,18 +6,10 @@ import Resolvers from './data/resolvers';
 import { apolloExpress, graphiqlExpress } from 'apollo-server';
 import { makeExecutableSchema } from 'graphql-tools';
 import bodyParser from 'body-parser';
->>>>>>> server-only
 
 const GRAPHQL_PORT = 8080;
 
 const graphQLServer = express();
-<<<<<<< HEAD
-graphQLServer.use('/graphql', apolloServer({
-  graphiql: true,
-  pretty: true,
-  schema: [schema],
-  mocks: Mocks,
-=======
 
 const executableSchema = makeExecutableSchema({
     typeDefs: Schema,
@@ -34,7 +21,6 @@ const executableSchema = makeExecutableSchema({
 graphQLServer.use('/graphql', bodyParser.json(), apolloExpress({
     schema: executableSchema,
     context: {}
->>>>>>> server-only
 }));
 
 graphQLServer.use('/graphiql', graphiqlExpress({
